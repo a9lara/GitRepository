a44c968b501aa8145e644654eff51d9adc0da88a
Armando J. Lara D.I <a9lara@gmail.com>
2015-03-19 23:03:48 -0600

    Un código para correr en Julia con funciónes y demás cosas y explicaciones

---
+++ FlatCodeJulia.jl
@@ -0,0 +1,10 @@
+#Notebook Random para correr en Julia#
+
+#Contiene cómo hacer varias cosas#
+
+#¿Cómo abrir un .jl desde julia?: include("/yourDirectory/hello.jl")#
+
+function sum(x,y)
+r=x+y
+println("¿Es en serio? ¿Necesitas de la computadora para saber cuánto es $x + $y? Pff... bueno, el resultado es $r")
+end
