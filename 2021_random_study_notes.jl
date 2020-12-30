b7e94d133574ed52cf334034abfaa16891187917
a9lara <a9lara@gmail.com>
2020-12-29 18:56:15 -0600

    updated

---
+++ 2021_random_study_notes.jl
@@ -0,0 +1,17 @@
+### A Pluto.jl notebook ###
+# v0.12.18
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 86bbd69a-4a38-11eb-2c1a-13c6e4a44d08
+md"
+# December 29, 2020
+
+ $\eta = \frac{\# \ baryons}{\# \ photons}$
+
+
+"
+
+# ╔═╡ Cell order:
+# ╠═86bbd69a-4a38-11eb-2c1a-13c6e4a44d08
